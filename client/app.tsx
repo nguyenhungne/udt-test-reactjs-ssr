aa6d7a23dea5058bea70bb808782f54e53e03fc2
nguyenhungne <nguyenhungf205@gmail.com>
2023-12-14 23:03:46 +0700

    set up prettier, Eslint and husky

--- client/app.tsx
+++ client/app.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import HomePage from "./pages/home";
-import { Route, Routes, Router } from 'react-router-dom';
+import { Route, Routes, Router } from "react-router-dom";
 import Calculator from "./pages/calculator";
 import HistoryPage from "./pages/history";
 
 function App() {
   return (
-      <>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/calculator" element={<Calculator />} />
-            <Route path="/history" element={<HistoryPage />} />
-          </Routes>
-      </>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+    </>
   );
 }
 
